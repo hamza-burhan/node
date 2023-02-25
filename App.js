67edc7dd47f34c974d7073bd46b86798df2309e4
Hamza Burhan <123304211+hamza-burhan@users.noreply.github.com>
2023-02-25 08:15:18 -0800

    node
    node t

---
+++ App.js
@@ -0,0 +1,18 @@
+
+
+
+//os node module  => os = require('os')
+//os.userinfo ,type(), release(), freemem(), totalmem(),  
+
+
+// path module => require('path')
+//path.sep(), join , basename(), resolve()
+
+
+//fs module
+// readfilesync(path,utf8), writefilesync(path,data,{flag,a}means it will append data not override)
+//readfile(path,utf8,(err,result)=>{})   writefile(path,data,{flag,a},(err, res)=>{}
+
+
+//http module
+//http.createServer((req,res)=>{res.end; req.url}).listen(5000)
