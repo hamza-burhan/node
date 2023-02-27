9a34629e6fc6ac2080bc395f2da0b9b875ef4672
Hamza Burhan <123304211+hamza-burhan@users.noreply.github.com>
2023-02-27 07:41:36 -0800

    final

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
 const express = require('express')
 const app = express()
-const path = require('path')
+
+const authr = require('./routes/auth.js')
 
 
-app.get('/',(req,res)=>{
-    app.use(express.static('./public'))
 
-    res.sendFile(path.resolve(__dirname,'./index.html'))
-    console.log("🚀 ~ file: App.js:8 ~ app.get ~ __dirname:", __dirname)
-    res.end
-})
+
+app.use('/login',authr)
+
+
+
+
 
 
 app.all('*',(req,res)=>{
     res.status(400).send('Error')
 })
-
 app.listen(2000,()=>{
     console.log('Server is Listining')
 })
@@ -129,3 +129,95 @@ app.listen(2000,()=>{
 //res.sendfile(path.resolve(__dirname,path))
 //app.use(express.static(folder path)) => send all static assets to the browser that will not 
 //change acording to req 
+
+
+
+//Api
+//send json data as a response
+//res.json(data in the form of json)
+
+
+//Route Params in Express
+//app.get('/about/:id')
+//id become param that we can get from user or route or URL
+//we can access it from req.params to write our own logic 
+//it return a string value
+
+
+
+//query string
+//user can send data from url
+//route?name=''&id='1'
+//we can get this data from req.query object
+
+
+
+//Middlewear
+// functions(any logic) execute between req => middleware => res
+//place app.get(path,middleware,callback)
+//function name(req,res,next)
+
+//app.use([middleware])
+//app.use(express.static(send the full folder  ))
+
+
+//req.user = blank object
+
+
+
+//POST
+//form actions='routes method='http method' if its post request
+//express post method
+//app.post(path,middleware,callback)
+//forms data goes to req.body
+//we can get this data from req.body by express middleware express.urlencoded({extended:true})
+//Now req.body has the data
+
+
+
+//if we want to access inputs data without sumitting form
+//val = input value
+//axios.post(path, data)
+//data will go to the req.body
+
+
+
+//app.use(express.json())
+//req.body has form data but we cant access json data posted 
+//.json() middleware will parse posted json data into req.body
+
+
+
+//posstman to test apis
+//morgan middleware to get time of server
+
+
+
+//we can give data to server by GET method
+//we can take data from a server by post method in req.body
+//we can post data by axios.post or javascript to post data to server
+
+
+
+//GET => to get resource from server
+//POST => to create new resource in server
+//PUT => to update current resource in server
+//DELETE => to delete resource from server
+//route could be same for all methods
+//ie app.get('/home') ,     app.post('/home')
+
+
+
+//express.Router
+//to separate routes from app.js
+//then use app.use('basepath', specific router)
+
+
+
+//controller folder
+//we write our api functions in controller folder and references them in routes folder
+
+
+
+
+ 
